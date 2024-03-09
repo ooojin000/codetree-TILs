a2fde9331e39404a84b2c6fda79049109e21f584
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2024-03-09 09:18:19 +0900

    Title: 전항의 두 배, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240309/전항의 두 배/twice-the-previous.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[11];
+
+        arr[1] = sc.nextInt();
+        arr[2] = sc.nextInt();
+
+        for(int i=3; i<11; i++) {
+            arr[i] = arr[i-1] + 2*arr[i-2];
+        }
+
+        for(int i=1; i<11; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
