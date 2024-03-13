813f0253cdbf538c7e2ff2010b365bbf91124e4e
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2024-03-13 22:46:24 +0900

    Title: 감소시키며 출력하기 2, Time: 103ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240313/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 10;
+        int i = 26;
+
+        while(i >= n) {
+            System.out.print(i + " ");
+            i--;
+        }
+    }
+}
