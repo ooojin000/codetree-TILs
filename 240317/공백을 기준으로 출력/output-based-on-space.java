9077c0580ec7350f18241e6daf80f6a8d9173420
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2024-03-17 16:18:17 +0900

    Title: 공백을 기준으로 출력, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240317/공백을 기준으로 출력/output-based-on-space.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+
+        for(int i=0; i<str1.length(); i++) {
+            if(str1.charAt(i) != ' ') {
+                System.out.print(str1.charAt(i));
+            }
+        }
+
+        for(int i=0; i<str2.length(); i++) {
+            if(str2.charAt(i) != ' ') {
+                System.out.print(str2.charAt(i));
+            }
+        }
+    }
+}
