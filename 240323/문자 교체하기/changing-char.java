4566e122c1f5baeae84fde94f9d265861a061c2d
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2024-03-23 21:59:06 +0900

    Title: 문자 교체하기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240323/문자 교체하기/changing-char.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+
+        str2 = str1.substring(0,2) + str2.substring(2);
+
+        System.out.print(str2);
+    }
+}
