fe39ecaadab0cb852eb1d94a4b1449b515dce6d1
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2024-03-23 23:30:50 +0900

    Title: 문자열 밀기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240323/문자열 밀기/push-char.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        int len = str.length();
+
+        str = str.substring(1, len) + str.substring(0, 1);
+
+        System.out.print(str);
+    }
+}
