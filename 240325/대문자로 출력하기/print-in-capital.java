1d39bb102928f8860ec526cd513684a3279957ff
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2024-03-25 08:29:10 +0900

    Title: 대문자로 출력하기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240325/대문자로 출력하기/print-in-capital.java
+++ 240325/대문자로 출력하기/print-in-capital.java
@@ -7,11 +7,11 @@ public class Main {
         char[] arr = str.toCharArray();
 
         for(int i = 0; i < arr.length; i++) {
-            if(arr[i] > 'A' && arr[i] < 'Z') {
+            if(arr[i] >= 'A' && arr[i] <= 'Z') {
                 System.out.print(arr[i]);
             }
 
-            if(arr[i] > 'a' && arr[i] < 'z') {
+            if(arr[i] >= 'a' && arr[i] <= 'z') {
                 System.out.print((char)(arr[i]-32));
             }
         }
