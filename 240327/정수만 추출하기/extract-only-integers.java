521bef31773ec938f5a27651d07a4fc4c994915e
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2024-03-27 23:47:36 +0900

    Title: 정수만 추출하기, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240327/정수만 추출하기/extract-only-integers.java
+++ 240327/정수만 추출하기/extract-only-integers.java
@@ -7,26 +7,30 @@ public class Main {
 
         char[] arr1 = str1.toCharArray();
         char[] arr2 = str2.toCharArray();
-        int cnt1=0;
-        int cnt2=0;
-        for( int i =0; i<str1.length(); i++){
-            if('0' <= arr1[i] && '9' >= arr1[i]){
+        int cnt1 = 0;
+        int cnt2 = 0;
+
+        for(int i = 0; i < str1.length(); i++) {
+            if('0' <= arr1[i] && '9' >= arr1[i]) {
                 cnt1++;
             }
-            else{
+            else {
                 break;
             }
         }
-        for( int j =0; j<str2.length(); j++){
-            if('0' <= arr2[j] && '9' >= arr2[j]){
+
+        for(int j = 0; j < str2.length(); j++) {
+            if('0' <= arr2[j] && '9' >= arr2[j]) {
                 cnt2++;
             }
-            else{
+            else {
                 break;
             }
         }
-        str1=str1.substring(0,cnt1);
-        str2=str2.substring(0,cnt2);
-        System.out.printf("%d",Integer.parseInt(str1)+Integer.parseInt(str2));
+
+        str1 = str1.substring(0,cnt1);
+        str2 = str2.substring(0,cnt2);
+        
+        System.out.printf("%d", Integer.parseInt(str1) + Integer.parseInt(str2));
     }
 }
