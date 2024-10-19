3c7a0baaa16aa2a4426ec8fb957c1521c7725c2c
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2024-10-19 14:42:10 +0900

    Title: 달에서 무게 구하기, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241019/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 13;
+        double b= 0.165;
+        System.out.printf("%.0f * %.6f = %.6f", a, b, a * b);
+    }
+}
