a46b3f4cfee7b1450e69e2aa1253b226ae66283c
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2024-11-02 22:20:30 +0900

    Title: 사칙연산 함수, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241102/사칙연산 함수/quadratic-operations-function.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        char o = sc.next().charAt(0);
+        int c = sc.nextInt();
+
+        calc(a, o, c);
+    }
+
+    public static void calc(int a, char o, int c) {
+        if (o == '+')
+            System.out.print(a + " + " + c + " = " + a + c);
+        else if (o == '-')
+            System.out.print(a + " - " + c + " = " + (a - c));
+        else if (o == '*') 
+            System.out.print(a + " * " + c + " = " + a * c);
+        else if (o == '/')
+            System.out.print(a + " / " + c + " = " + a / c);
+        else 
+            System.out.print("False");
+    }
+}
