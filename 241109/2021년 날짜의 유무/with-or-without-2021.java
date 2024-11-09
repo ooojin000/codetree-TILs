a6e901a50346caf8026ede9a1c7d31594532d232
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2024-11-09 20:18:33 +0900

    Title: 2021년 날짜의 유무, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 241109/2021년 날짜의 유무/with-or-without-2021.java
+++ 241109/2021년 날짜의 유무/with-or-without-2021.java
@@ -1,34 +1,33 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
-        int d = sc.nextInt();
-        month(m, d);
+public class Main {    
+    // 윤년이 아닐 때 m번째 달의 마지막 날을 반환하는 함수를 작성합니다.
+    public static int lastDayNumber(int m) {
+        if(m == 2)
+            return 28;
+        if(m == 4 || m == 6 || m == 9 || m == 11)
+            return 30;
+        
+        return 31;
+    }
+    
+    // 윤년이 아닐 때 m월 d일이 존재하는지 여부를 확인하는 함수를 작성합니다.
+    public static boolean judgeDay(int m, int d) {
+        if(m <= 12 && d <= lastDayNumber(m))
+            return true;
+        
+        return false;
     }
 
-    public static void month(int m, int d) {
-        if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
-            if (d >= 1 && d <= 31) {
-                System.out.print("Yes");
-            } else {
-                System.out.print("No");
-            }
-        } else if (m == 4 || m == 6 || m == 9 || m == 11) {
-            if (d >= 1 && d <= 30) {
-                System.out.print("Yes");
-            } else {
-                System.out.print("No");
-            }
-        } else if (m == 2) {
-            if (d >= 1 && d <= 28) {
-                System.out.print("Yes");
-            } else {
-                System.out.print("No");
-            }
-        } else {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력:
+        int m = sc.nextInt();
+        int d = sc.nextInt();
+        
+        if(judgeDay(m, d))
+            System.out.print("Yes");
+        else
             System.out.print("No");
-        }
     }
 }
