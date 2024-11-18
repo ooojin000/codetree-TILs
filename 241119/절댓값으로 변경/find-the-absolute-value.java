ce6575e5857115753d3f5e83d8f3e0d44a061433
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2024-11-19 04:31:31 +0900

    Title: 절댓값으로 변경, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241119/절댓값으로 변경/find-the-absolute-value.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        absolute(arr);
+    }
+
+    public static void absolute(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                arr[i] = Math.abs(arr[i]);
+            }
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
