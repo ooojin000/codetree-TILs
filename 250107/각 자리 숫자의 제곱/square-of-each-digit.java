ea656ed0d22c62b83a3d8ea2330773e1d3b921e7
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-01-07 16:13:50 +0900

    Title: 각 자리 숫자의 제곱, Time: 109ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250107/각 자리 숫자의 제곱/square-of-each-digit.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.print(F(n));
+    }
+
+    public static int F(int n) {
+        if(n < 10)
+            return n * n;
+
+        return F(n / 10) + (n % 10) * (n % 10);
+    }
+}
