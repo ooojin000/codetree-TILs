1e596043ac13c87723aefbd21c6e4d2d258ca6be
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-02-08 20:27:20 +0900

    Title: 3N + 1 수열, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250208/3N + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int cnt = 0;
+        
+        while (n > 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+                cnt++;
+            } else {
+                n = n * 3 + 1;
+                cnt++;
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}
