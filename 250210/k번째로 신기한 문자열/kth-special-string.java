b93f91b4f2915bfc10774a81a97687e3fc81253a
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-02-10 07:29:28 +0900

    Title: k번째로 신기한 문자열, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250210/k번째로 신기한 문자열/kth-special-string.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        String str1 = sc.next();
+
+        String[] arr1 = new String[n];
+
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++) {
+            arr1[i] = sc.next();
+
+            if(arr1[i].length() > str1.length()) {
+                String str2 = arr1[i].substring(0, str1.length());  
+
+                if(str2.equals(str1)) {
+                    cnt++;
+                }
+            }
+            
+        }
+
+        int index = 0;
+
+        String[] arr2 = new String[cnt];
+
+        for(int i = 0; i < n; i++) {
+            if(arr1[i].length() > str1.length()) {
+                String str2 = arr1[i].substring(0, str1.length());
+            
+                if(str2.equals(str1)) {
+                    arr2[index++] = arr1[i];
+                }
+            }
+        }
+
+        Arrays.sort(arr2);
+
+        System.out.print(arr2[k - 1]);
+
+        
+
+    }
+}
