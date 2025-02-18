1e900dcae64fc646f0daac30203684a690ac1fb5
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-02-19 00:29:34 +0900

    Title: 개인정보, Time: 151ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 250219/개인정보/personal-info.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Student[] students = new Student[5];
+
+        for(int i = 0; i < 5; i++) {
+            String name = sc.next();
+            int height = sc.nextInt();
+            float weight = sc.nextFloat();
+            students[i] = new Student(name, height, weight);
+        }
+
+        Arrays.sort(students);
+
+        System.out.println("name");
+
+        for(int i = 0; i < 5; i++) {
+            System.out.print(students[i].name + " ");
+            System.out.print(students[i].height + " ");
+            System.out.println(students[i].weight + " ");
+        }
+
+        Arrays.sort(students, new Comparator<Student>() {
+            @Override
+            public int compare(Student s1, Student s2) {
+                return Integer.compare(s2.height, s1.height);
+            }
+        });
+
+        System.out.println();
+        System.out.println("height");
+
+        for(int i = 0; i < 5; i++) {
+            System.out.print(students[i].name + " ");
+            System.out.print(students[i].height + " ");
+            System.out.println(students[i].weight + " ");
+        }
+    }
+}
+
+class Student implements Comparable<Student> {
+    String name;
+    int height;
+    float weight;
+
+    public Student(String name, int height, float weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        return this.name.compareTo(student.name);
+    }
+}
