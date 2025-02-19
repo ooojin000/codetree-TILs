abd239130776fe31c9a27667d0d965a4ed9a6f44
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-02-19 09:17:33 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 286ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250219/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Student[] students = new Student[n];
+
+        for(int i = 0; i < n; i++) {
+            String name = sc.next();
+            int height = sc.nextInt();
+            int weight = sc.nextInt();
+            students[i] = new Student(name, height, weight);
+        }
+
+        Arrays.sort(students, (a, b) -> {
+            if(a.height != b.height) 
+                return a.height - b.height;
+            else
+                return b.weight - a.weight;
+        });
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(students[i].name + " ");
+            System.out.print(students[i].height + " ");
+            System.out.println(students[i].weight + " ");
+        }
+    }
+}
+
+class Student {
+    String name;
+    int height, weight;
+
+    public Student(String name, int height, int weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+}
