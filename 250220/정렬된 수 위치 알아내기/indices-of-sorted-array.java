6cd0b6a4936ba1fb0355024c17bd2ceeef8c2f82
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-02-20 15:58:02 +0900

    Title: 정렬된 수 위치 알아내기, Time: 244ms, Memory: 14MB, Status: Passed - CodeTree

--- 250220/정렬된 수 위치 알아내기/indices-of-sorted-array.java
+++ 250220/정렬된 수 위치 알아내기/indices-of-sorted-array.java
@@ -4,16 +4,44 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        Location[] locations = new Location[n];
 
         for(int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            int x = sc.nextInt();
+            int num = i + 1;
+            locations[i] = new Location(x, num);
         }
 
-        Arrays.sort(arr);
+        Arrays.sort(locations);
 
         for(int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
+            locations[i].newNum = i + 1;
         }
+
+        Arrays.sort(locations, new Comparator<Location>() {
+            @Override
+            public int compare(Location a, Location b) {
+                return a.num - b.num;
+            }
+        });
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(locations[i].newNum + " ");
+        }
+    }
+}
+
+class Location implements Comparable<Location> {
+    int x, num, newNum;
+
+    public Location(int x, int num) {
+        this.x = x;
+        this.num = num;
+        this.newNum = newNum;
+    }
+
+    @Override
+    public int compareTo(Location location) {
+        return this.x - location.x;
     }
 }
