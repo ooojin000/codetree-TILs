b1381c9431cff8cd1a1fdf00b2c0e5730c5d8a94
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-04-07 22:02:41 +0900

    Title: 삼항연산자, Time: 140ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250407/삼항연산자/ternary-operator.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        String str;
+
+        str = n == 100 ? "pass" : "failure";
+        
+        System.out.print(str);
+        
+    }
+}
