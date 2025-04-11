d765d4a0a02f6c3b95106087c5fbbcaba03511a1
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-04-11 23:13:43 +0900

    Title: 변수 값 복사하기 3, Time: 116ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250411/변수 값 복사하기 3/copying-variable-values-3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 1;
+        int b = 5;
+        int c = 3;
+
+        a = c;
+        a = a + c;
+        b = b - c;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
