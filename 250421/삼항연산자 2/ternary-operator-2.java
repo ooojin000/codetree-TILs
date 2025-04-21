98ab2897c3b862883e51edac6a5ef2b4a0d913e1
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-04-21 23:23:35 +0900

    Title: 삼항연산자 2, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250421/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,13 @@
+import java.util*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a == 1) {
+            System.out.print("t");
+        } else {
+            System.out.print("f");
+        }
+    }
+}
