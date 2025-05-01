93be59358530e3100c90effea07d66b6ed1d08be
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-05-02 08:02:33 +0900

    Title: 여러가지 진수변환, Time: 109ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250502/여러가지 진수변환/various-numeral-system-transformations.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+        int cnt = 0;
+        int[] digits = new int[20];
+
+        while(true) {
+            if(n < b) {
+                digits[cnt++] = n;
+                break;
+            }
+            
+            digits[cnt++] = n % b;
+            n /= b;
+        }
+        
+        for(int i = cnt - 1; i >= 0; i--)
+            System.out.print(digits[i]);
+    }
+}
