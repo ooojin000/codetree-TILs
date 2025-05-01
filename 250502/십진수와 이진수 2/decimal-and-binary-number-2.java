bc379f610bc79cba1cb560aceb6401a8fea20053
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-05-02 08:19:31 +0900

    Title: 십진수와 이진수 2, Time: 113ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250502/십진수와 이진수 2/decimal-and-binary-number-2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String binary = sc.next();
+
+        int[] digits = new int[20];
+
+        int num = 0;
+        for(int i = 0; i < binary.length(); i++)
+            num = num * 2 + (binary.charAt(i) - '0');
+        
+        num *= 17;
+        int cnt = 0;
+
+        while (true) {
+            if(num < 2) { 
+                digits[cnt++] = num;
+                break;
+            }
+            digits[cnt++] = num % 2;
+            num /= 2;
+        }
+        
+        for(int i = cnt - 1; i >= 0; i--) {
+            System.out.print(digits[i]);
+        }
+    }
+}
