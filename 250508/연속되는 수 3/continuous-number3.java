25a36e9241b88b3cb04a4354ab8c0b2b06c366ac
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-05-08 08:40:13 +0900

    Title: 연속되는 수 3, Time: 187ms, Memory: 11MB, Status: Passed - Codetree

--- 250508/연속되는 수 3/continuous-number3.java
+++ 250508/연속되는 수 3/continuous-number3.java
@@ -12,9 +12,7 @@ public class Main {
         int max = 0;
 
         for (int i = 0; i < n; i++) {
-            if (i >= 1 && arr[i] > 0 && arr[i - 1] > 0)
-                cnt++;
-            else if (i >= 1 && arr[i] < 0 && arr[i - 1] < 0)
+            if (i >= 1 && arr[i] * arr[i - 1] > 0)
                 cnt++;
             else
                 cnt = 1;
