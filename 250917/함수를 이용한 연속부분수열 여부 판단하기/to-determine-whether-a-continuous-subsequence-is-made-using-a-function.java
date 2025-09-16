2417a364b6c26e03105da27ce5b575ac680fd95e
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-09-17 05:24:45 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250917/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int[] a = new int[n1];
+        int[] b = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            a[i] = sc.nextInt();
+
+        for (int i = 0; i < n2; i++)
+            b[i] = sc.nextInt();
+
+        boolean result = false;
+        
+        for (int i = 0; i < n1 - n2; i++) {
+            if (a[i] == b[0]) {
+                result = true;
+                for (int j = 0; j < n2; j++) {
+                    if (a[i + j] != b[j]) {
+                        result = false;
+                    }
+                }
+            }
+        }
+
+        System.out.print(result == true ? "Yes" : "No");
+    }
+}
