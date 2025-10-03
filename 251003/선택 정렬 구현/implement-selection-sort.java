ff5fb8cb7228b1bc92a0eec624c462c7a5ac15cd
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-10-03 22:33:46 +0900

    Title: 선택 정렬 구현, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251003/선택 정렬 구현/implement-selection-sort.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int min, tmp = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            tmp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = tmp;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
