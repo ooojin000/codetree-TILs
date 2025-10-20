153661fe14539614f3ac629474e4055598c1fec3
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-10-21 03:12:00 +0900

    Title: 수열 조작, Time: 197ms, Memory: 13MB, Status: Passed - Codetree

---
+++ 251021/수열 조작/sequence-manipulation.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Deque<Integer> deque = new ArrayDeque<>();
+        
+        for (int i = 1; i <= n; i++) {
+            deque.addLast(i);
+        }
+
+        while (deque.size() > 1) {
+            deque.pollFirst();
+            deque.addLast(deque.pollFirst());
+        }
+
+        System.out.print(deque.peekFirst());
+    }
+}
