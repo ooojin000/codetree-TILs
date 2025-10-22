46068bfb907c835e8d43282621f3be6414d28be6
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-10-22 12:20:04 +0900

    Title: 요일 맞추기, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

--- 251022/요일 맞추기/guess-day-of-week.java
+++ 251022/요일 맞추기/guess-day-of-week.java
@@ -7,33 +7,26 @@ public class Main {
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
 
-
-        int date1 = elapsedDays(m1, d1);
-        int date2 = elapsedDays(m2, d2);
-
-        int diff = date2 - date1;
+        int diff = elapsedDays(m2, d2) - elapsedDays(m1, d1);
 
         while(diff < 0) {
             diff += 7;
         }
-
         
-        String[] day_of_the_week = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-
-        System.out.print(day_of_the_week[diff & 7]);
+        String[] dayOfWeek = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        System.out.print(dayOfWeek[diff % 7]);
         
     }
 
     public static int elapsedDays(int m, int d) {
-        int elapsedDays = 0;
-
-        int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int totalDays = 0;
+        int[] days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
         for(int i = 0; i < m; i++) {
-            elapsedDays += num_of_days[i];
+            totalDays += days[i];
         }
-        elapsedDays += d;
+        totalDays += d;
         
-        return elapsedDays;
+        return totalDays;
     }
 }
