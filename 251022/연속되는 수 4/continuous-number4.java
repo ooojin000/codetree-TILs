bca50e4514230d9679096e79f83710798a427aaf
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-10-22 14:16:27 +0900

    Title: 연속되는 수 4, Time: 146ms, Memory: 10MB, Status: Passed - Codetree

--- 251022/연속되는 수 4/continuous-number4.java
+++ 251022/연속되는 수 4/continuous-number4.java
@@ -9,7 +9,7 @@ public class Main {
         }
         
         int cnt = 1;
-        int max = 0;
+        int max = 1;
 
         for (int i = 0; i < n - 1; i++) {
             if (arr[i] < arr[i + 1]) {
