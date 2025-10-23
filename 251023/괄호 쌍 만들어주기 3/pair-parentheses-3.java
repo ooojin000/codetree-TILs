17bf15acee641a5c338c411c2c3e2da52a49d9b5
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-10-23 16:46:51 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251023/괄호 쌍 만들어주기 3/pair-parentheses-3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        
+        char[] cArr = str.toCharArray();
+
+        int cnt = 0;
+        
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (cArr[i] == '(')
+            for (int j = i + 1; j < str.length(); j++) {
+                if (cArr[j] == ')') {
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}
